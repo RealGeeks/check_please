af74d1f0a85378da15cb0a9829283ec1371eef81
Sam Livingston-Gray <geeksam@gmail.com>
2020-12-30 17:55:11 -0800

    WIP: end of day; maybe better?

--- lib/check_please/path.rb
+++ lib/check_please/path.rb
@@ -1,21 +1,108 @@
 module CheckPlease
 
   class Path
+    class AbstractSegment
+      def self.new(name_or_instance = nil)
+        case name_or_instance
+        when self        ; name_or_instance
+        when String, nil ; super
+        else             ; raise ArgumentError
+        end
+      end
+
+      attr_reader :name
+      alias_method :to_s, :name
+
+      def initialize(name)
+        @name = name.to_s.strip
+        complain_about_invalid_name! if @name =~ %r(\s)
+        freeze
+        validate_name!
+      end
+
+      def empty?
+        name.empty?
+      end
+
+      private
+
+      def validate_name!
+        # subclass may override
+      end
+
+      def complain_about_invalid_name!
+        msg = "#{name.inspect} is not a valid #{self.class} name!"
+        raise ArgumentError, msg
+      end
+    end
+
+    class Segment < AbstractSegment
+      def key
+        return nil unless name =~ /^([^=]+)=/
+        $1.to_sym
+      end
+
+      def match?(segment_expr)
+        if segment_expr.to_s.count(":").zero?
+          return name == segment_expr.to_s # the easy way!
+        else
+          SegmentExpr.new(segment_expr).match?(name)
+        end
+      end
+
+      private
+
+      def validate_name!
+        complain_about_invalid_name! if name.include?(":")
+      end
+    end
+
+    class SegmentExpr < AbstractSegment
+      def match?(segment_name)
+        !!( segment_name.to_s =~ /^#{key}=/ )
+      end
+
+      def key
+        name.sub(/^:/, "").to_sym
+      end
+
+      private
+
+      def validate_name!
+        complain_about_invalid_name! unless name.start_with?(":")
+        complain_about_invalid_name! unless name.count(":") == 1
+      end
+    end
+  end
+
+  class Path
     SEPARATOR = "/"
 
     def self.root
       new
     end
 
-    attr_reader :to_s
+    attr_reader :to_s, :segments
     def initialize(segments = [])
-      @segments = Array(segments)
+      case segments
+      when String
+        @segments = segments.split(SEPARATOR)
+        @segments.shift until @segments.empty? || @segments.first.length > 0
+      when nil, Array
+        @segments = Array(segments)
+      else
+        raise ArgumentError, "not sure what to do with #{segments.inspect}"
+      end
       @to_s = SEPARATOR + @segments.join(SEPARATOR)
       freeze
     end
 
     def +(new_basename)
       self.class.new( Array(@segments) + Array(new_basename.to_s) )
+    end
+
+    def basename
+      segments.last.to_s
     end
 
     def depth
@@ -60,6 +147,10 @@ module CheckPlease
       return false if flags.max_depth.nil?
       flags.max_depth + 1 < depth
     end
+  end
+
+  class PathExpr < Path
+
   end
 
 end
--- spec/check_please/path_spec.rb
+++ spec/check_please/path_spec.rb
@@ -1,24 +1,404 @@
 module CheckPlease
+  RSpec.shared_examples "AbstractSegment" do
+    describe ".new (from AbstractSegment)" do
+      it "returns the instance when given an instance of itself" do
+        returned = described_class.new(valid_instance)
+        expect( returned ).to be( valid_instance ) # object identity check
+      end
+
+      it "raises ArgumentError when given an Integer" do
+        expect { described_class.new(42) }.to \
+          raise_error( ArgumentError )
+      end
+
+      it "raises ArgumentError when given a string containing a space between non-space characters " do
+        expect { described_class.new("hey bob") }.to \
+          raise_error( ArgumentError )
+      end
+    end
+  end
+
+  module ItReturnsForMatchEh
+    def it_returns(expected, when_given:)
+      it "returns #{expected.inspect} when given #{when_given.inspect}" do
+        actual = subject.match?(when_given)
+        case actual
+        when true, false, nil
+          expect( actual ).to be( expected ) # identity
+        else
+          expect( actual ).to eq( expected ) # equality
+        end
+      end
+    end
+  end
+
+  RSpec.describe Path::Segment do
+    include_examples "AbstractSegment" do
+      let(:valid_instance) { described_class.new("foo") }
+    end
+
+    describe ".new" do
+      it "returns an instance with name='' when given no arguments" do
+        seg = described_class.new()
+        expect( seg ).to be_a(described_class)
+        expect( seg.name ).to eq( "" )
+      end
+
+      it "returns an instance with name='foo' when given 'foo'" do
+        seg = described_class.new("foo")
+        expect( seg ).to be_a(described_class)
+        expect( seg.name ).to eq( "foo" )
+      end
+
+      it "returns an instance with name='foo' when given '   foo '" do
+        seg = described_class.new("   foo ")
+        expect( seg ).to be_a(described_class)
+        expect( seg.name ).to eq( "foo" )
+      end
+
+      it "raises ArgumentError when given a string that DOES contain a colon" do
+        expect { described_class.new(":id") }.to \
+          raise_error( ArgumentError )
+      end
+    end
+
+    describe "#key" do
+      it "returns nil when name='foo'" do
+        subject = described_class.new("foo")
+        expect( subject.key ).to be nil
+      end
+
+      it "returns :id when name='id=42'" do
+        subject = described_class.new("id=42")
+        expect( subject.key ).to eq( :id )
+      end
+
+      # REMINDER: name=':id' is invalid
+    end
+
+    describe "#match?" do
+      extend ItReturnsForMatchEh
+
+      context "with name='foo'" do
+        subject { described_class.new("foo") }
+
+        it_returns true,  when_given: "foo"
+        it_returns false, when_given: "bar"
+        it_returns false, when_given: ":id"
+        it_returns false, when_given: "id=23"
+        it_returns false, when_given: "id=42"
+
+        it_returns false, when_given: Path::SegmentExpr.new(":id")
+      end
+
+      context "with name='id=42'" do
+        subject { described_class.new("id=42") }
+
+        it_returns false, when_given: "foo"
+        it_returns false, when_given: "bar"
+        it_returns true,  when_given: ":id"
+        it_returns false, when_given: "id=23"
+        it_returns true,  when_given: "id=42"
+
+        it_returns true,  when_given: Path::SegmentExpr.new(":id")
+      end
+
+      # REMINDER: name=':id' is invalid
+    end
+  end
+
+  RSpec.describe Path::SegmentExpr do
+    include_examples "AbstractSegment" do
+      let(:valid_instance) { described_class.new(":id") }
+    end
+
+    describe ".new" do
+      it "raises ArgumentError when given a string that DOES NOT contain a colon" do
+        expect { described_class.new("id") }.to \
+          raise_error( ArgumentError )
+      end
+
+      it "raises ArgumentError when given a string that DOES NOT contain a LEADING colon" do
+        expect { described_class.new("id:") }.to \
+          raise_error( ArgumentError )
+        expect { described_class.new("id:wibble") }.to \
+          raise_error( ArgumentError )
+      end
+
+      it "raises ArgumentError when given a string that contains more than one colon" do
+        expect { described_class.new(":id:wibble") }.to \
+          raise_error( ArgumentError )
+      end
+    end
+
+    describe "#key" do
+      it "returns #name as a Symbol with the initial colon removed" do
+        segex = described_class.new(":id")
+        expect( segex.key ).to eq( :id )
+      end
+    end
+
+    describe "#match?" do
+      extend ItReturnsForMatchEh
+
+      # REMINDER: name='foo' is invalid
+      # REMINDER: name='id=42' is also invalid
+
+      context "for a segment with name=':id'" do
+        subject { described_class.new(":id") }
+
+        it_returns false, when_given: "foo"
+        it_returns false, when_given: "bar"
+        it_returns false, when_given: ":id"
+        it_returns true,  when_given: "id=23"
+        it_returns true,  when_given: "id=42"
+
+        it_returns false, when_given: Path::Segment.new("foo")
+        it_returns false, when_given: Path::Segment.new("bar")
+        # not testing Path::Segment.new(":id") because it's invalid
+        it_returns true,  when_given: Path::Segment.new("id=23")
+        it_returns true,  when_given: Path::Segment.new("id=42")
+      end
+    end
+  end
+
   RSpec.describe Path do
     let(:root) { Path.new }
 
-    specify "a new Path describes itself as '/'" do
-      expect( root.to_s ).to eq( "/" )
+    describe "a new Path with no segments" do
+      it "describes itself as '/'" do
+        expect( root.to_s ).to eq( "/" )
+      end
+
+      it "indeed has no segments" do
+        expect( root.segments ).to be_empty
+      end
+
+      it "has a depth of 1" do
+        expect( root.depth ).to eq( 1 )
+      end
+
+      it "answers true for #root?" do
+        expect( root.root? ).to be true
+      end
     end
 
-    specify "a path plus a string is a new path with the string added as the last segment" do
-      child = root + 'wibble'
-      expect( child ).to_not be( root )
-      expect( child.to_s ).to eq( "/wibble" )
+    describe "a Path plus a string" do
+      let(:new_path) { root + "wibble" }
+
+      it "is a new Path with the string added" do
+        expect( new_path      ).to_not be( root )
+        expect( new_path.to_s ).to     eq( "/wibble" )
+      end
+
+      it "has a #basename of the string" do
+        expect( new_path.basename ).to eq( "wibble" )
+      end
+
+      it "has a #depth of 2" do
+        expect( new_path.depth ).to eq( 2 )
+      end
     end
 
-    specify "a root path has a depth of 1" do
-      expect( root.depth ).to eq( 1 )
+    specify "a Path can be created from a string with no slashes" do
+      path = Path.new("foo")
+      expect( path.to_s  ).to eq( "/foo" )
+      expect( path.depth ).to eq( 2 )
     end
 
-    specify "a child of the root has a #depth of 2" do
-      child = root + 'wibble'
-      expect( child.depth ).to eq( 2 )
+    specify "a Path can be created from a string with a leading slash" do
+      path = Path.new("/foo")
+      expect( path.to_s  ).to eq( "/foo" )
+      expect( path.depth ).to eq( 2 )
+    end
+
+    specify "a longer Path can be created from a string with multiple slashes" do
+      path = Path.new("/foo/bar/yak")
+      expect( path.to_s  ).to eq( "/foo/bar/yak" )
+      expect( path.depth ).to eq( 4 )
+    end
+
+    specify "a path of '/foo/id=42' is valid" do
+      path = Path.new("/foo/id=42")
+      expect( path.to_s  ).to eq( "/foo/id=42" )
+      expect( path.depth ).to eq( 3 )
+    end
+
+    specify "a path of '/foo/:id' is NOT valid" do
+      expect { Path.new("/foo/:id") }.to raise_error( ArgumentError )
     end
   end
+
+  RSpec.describe PathExpr do
+    let(:root) { PathExpr.new }
+
+    describe "a new PathExpr with no segments" do
+      it "describes itself as '/'" do
+        expect( root.to_s ).to eq( "/" )
+      end
+
+      it "indeed has no segments" do
+        expect( root.segments ).to be_empty
+      end
+
+      it "has a depth of 1" do
+        expect( root.depth ).to eq( 1 )
+      end
+
+      it "answers true for #root?" do
+        expect( root.root? ).to be true
+      end
+    end
+
+    describe "a PathExpr plus a string" do
+      let(:new_path) { root + "wibble" }
+
+      it "is a new PathExpr with the string added" do
+        expect( new_path      ).to_not be( root )
+        expect( new_path.to_s ).to     eq( "/wibble" )
+      end
+
+      it "has a #basename of the string" do
+        expect( new_path.basename ).to eq( "wibble" )
+      end
+
+      it "has a #depth of 2" do
+        expect( new_path.depth ).to eq( 2 )
+      end
+    end
+
+    specify "a PathExpr can be created from a string with no slashes" do
+      path = PathExpr.new("foo")
+      expect( path.to_s  ).to eq( "/foo" )
+      expect( path.depth ).to eq( 2 )
+    end
+
+    specify "a PathExpr can be created from a string with a leading slash" do
+      path = PathExpr.new("/foo")
+      expect( path.to_s  ).to eq( "/foo" )
+      expect( path.depth ).to eq( 2 )
+    end
+
+    specify "a longer PathExpr can be created from a string with multiple slashes" do
+      path = PathExpr.new("/foo/bar/yak")
+      expect( path.to_s  ).to eq( "/foo/bar/yak" )
+      expect( path.depth ).to eq( 4 )
+    end
+
+    specify "a path of '/foo/id=42' is NOT valid" do
+      expect { Path.new("/foo/id=42") }.to raise_error( ArgumentError )
+    end
+
+    specify "a PathExpr of '/foo/:id' is valid" do
+      path = Path.new("/foo/:id")
+      expect( path.to_s  ).to eq( "/foo/:id" )
+      expect( path.depth ).to eq( 3 )
+    end
+  end
+
+=begin
+  RSpec.describe Path do
+    describe ".segment_matches? (with segment followed by segment_expr)" do
+      def invoke!(segment, segment_expr)
+        Path.segment_matches?( segment: segment, segment_expr: segment_expr )
+      end
+
+      it "answers true for [ 'foo', 'foo' ]" do
+        expect( invoke!( "foo", "foo" ) ).to be true
+      end
+
+      it "answers true for [ 'id=42', ':id' ]" do
+        expect( invoke!( "id=42", ":id" ) ).to be true
+      end
+
+      it "answers false for [ 'foo', 'bar' ]" do
+        expect( invoke!( "foo", "bar" ) ).to be false
+      end
+
+      it "answers false for [ 'name=bob', ':id' ]" do
+        expect( invoke!( "name=bob", ":id" ) ).to be false
+      end
+
+      it "complains for [ 'id=42', ':id:baby' ]" do
+        expect { invoke!( "id=42", ":id:baby" ) }.to \
+          raise_error( ArgumentError )
+      end
+    end
+
+    describe "#matches_path_expr?" do
+      def invoke!(path_string, path_expr)
+        raise ArgumentError, "No leading slash in path_string: #{path_string.inspect}" unless path_string.start_with?("/")
+        raise ArgumentError, "No leading slash in path_expr: #{path_expr.inspect}" unless path_string.start_with?("/")
+        path = Path.new( path_string )
+        path.matches_path_expr?(path_expr)
+      end
+
+      it "answers true for a simple path that matches the path expression literally" do
+        expect( invoke!( "/foo", "/foo" ) ).to be true
+      end
+
+      it "answers true for a simple path that matches a path expression on key" do
+        expect( invoke!( "/id=42", "/:id" ) ).to be true
+      end
+
+      it "answers true for a longer path that matches a path expression on key" do
+        expect( invoke!( "/foo/bar/id=42", "/foo/bar/:id" ) ).to be true
+      end
+
+      it "answers true for a path with two keys in it" do
+        expect( invoke!( "/foo/id=1/bar/id=2", "/foo/:id/bar/:id" ) ).to be true
+      end
+
+      it "answers false for a simple path that doesn't match the path expression literally" do
+        expect( invoke!( "/foo", "/bar" ) ).to be false
+      end
+
+      it "answers false for a path that is longer than the path expression" do
+        expect( invoke!( "/foo/bar", "/bar" ) ).to be false
+      end
+
+      it "answers false for a path that is shorter than the path expression" do
+        expect( invoke!( "/foo", "/foo/bar" ) ).to be false
+      end
+
+      it "answers false for a simple path that doesn't match the path expression by key" do
+        expect( invoke!( "/name=bob", "/:id" ) ).to be false
+      end
+
+      it "answers false for a longer path that doesn't match the path expression by key" do
+        expect( invoke!( "/foo/bar/name=bob", "/foo/bar/:id" ) ).to be false
+      end
+
+      it "answers false for a path with two keys in it that doesn't match the second path expression by key" do
+        expect( invoke!( "/foo/id=1/bar/name=bob", "/foo/:id/bar/:id" ) ).to be false
+      end
+
+      it "complains if a segment in the path expression contains more than one colon" do
+        expect { invoke!( "/id=42",         "/:id:baby" )         }.to raise_error( ArgumentError )
+        expect { invoke!( "/foo/bar/id=42", "/foo/bar/:id:baby" ) }.to raise_error( ArgumentError )
+      end
+    end
+
+    describe "#key_name_for_match" do
+      specify "'/' answers :id for '/:id'" do
+        path = Path.new('/')
+        flags = Flags.new(match_by_key: [ '/:id' ])
+        expect( path.key_name_for_match(flags) ).to eq( :id )
+      end
+
+      specify "'/foo' answers :id for '/foo/:id'" do
+        path = Path.new('/foo')
+        flags = Flags.new(match_by_key: [ '/foo/:id' ])
+        expect( path.key_name_for_match(flags) ).to eq( :id )
+      end
+
+      specify "'/foo/bar' answers :id for '/foo/bar/:id'" do
+        fail "write me"
+      end
+
+      specify "'/foo/bar/id=42/yak' answers :id2 for '/foo/bar/:id1/yak/:id2'" do
+        fail "write me"
+      end
+    end
+=end
 end
